7a3cd700f6b7729d69ff7be61f030826f8354359
Francis <yaowen021316@hotmail>
2022-01-19 00:15:10 +1100

    fix: remove files

--- src/context/contextProvider.tsx
+++
@@ -1,8 +0,0 @@
-import React, { ReactNode } from 'react'
-import { AuthProvider } from '../context/AuthProvider'
-
-const AuthProviders = ({ children }: { children: ReactNode }) => {
-	return <AuthProvider>{children}</AuthProvider>
-}
-
-export default AuthProviders
